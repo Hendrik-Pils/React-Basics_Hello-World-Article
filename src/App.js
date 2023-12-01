3aaad36551aa94191743efcffdab33bb42718ae0
Hendrik Pils <hendrik-pils@gmx.de>
2023-12-01 12:20:09 +0100

    finish challenge

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1 className="h1">Hello World</h1>
+      <p className="p">Hallo!</p>
+      <p className="p">Hallo 2</p>
+    </article>
+  );
 }
